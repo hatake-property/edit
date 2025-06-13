53ec83685aefc6a473d88ee8cdab3e8d2b2c2520
hatake <hermit183@outlook.com>
2025-06-13 11:05:02 +0900

    set path and content

--- edit.bash
+++ edit.bash
@@ -1,9 +1,35 @@
 #!/bin/bash
 
 cmd=
+# file information
+path=
+content=
 
-while true; do
-	read -s -n 1 key
+if [ $# -eq 0 ]
+then
+	for (( i=0 ; true ; i++ ))
+	do
+		if [ -e "untitled-$i" ]
+		then
+		elif
+			path="untitled-$i"
+			break
+		fi
+	done
+	content=""
+elif
+	path="$1"
+	if [ -e "$path" ]
+	then
+		content=$(<"$path")
+	elif
+		content=""
+	fi
+fi
+
+while true
+do
+	read -s -r -n 1 key
 	cmd+="$key"
 	case "$cmd" in
 	[Qq])
