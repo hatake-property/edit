d895ddac19d675676def160c160b6ecb80a9550d
hatake <hermit183@outlook.com>
2025-06-13 12:53:31 +0900

    divide token

--- edit.bash
+++ edit.bash
@@ -3,32 +3,59 @@
 cmd=
 # file information
 path=
-content=
+token=()
 
 if [ $# -eq 0 ]
 then
 	for (( i=0 ; true ; i++ ))
 	do
-		if [ -e "untitled-$i" ]
-		then
-		elif
+		if [ ! -e "untitled-$i" ]
+        then
 			path="untitled-$i"
 			break
 		fi
 	done
 	content=""
-elif
+else
 	path="$1"
 	if [ -e "$path" ]
 	then
-		content=$(<"$path")
-	elif
+		s=
+		for (( i=1 ; i -le ${#$(<"$path")} ; i++ ))
+		do
+			c=$(<"$path")|cat -c i
+			case "$c" in
+			\n)
+				token+=("$s")
+				s+="$c"
+				;;
+			[\s])
+				if [ "$s"|cat -c 0 != [ ] ]
+				then
+					token+=("$s")
+					s=""
+				fi
+				s+="$c"
+				;;
+			*)
+				if [ "$s"|cat -c 0 = [ ] ] || [ "$s"|cat -c 0 = [ ] ]
+				then
+					token+=("$s")
+					s=""
+				fi
+				s+="$c"
+				;;
+			esac
+		done
+		token+=("$s")
+	else
 		content=""
 	fi
 fi
 
 while true
 do
+	clear
 	read -s -r -n 1 key
 	cmd+="$key"
 	case "$cmd" in
